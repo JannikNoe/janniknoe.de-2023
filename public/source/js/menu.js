d075267a2963440cc963395f7b77f1ebcaef6994
JannikNoe <jannik.noe@gmail.com>
2023-01-02 02:17:12 +0100

    Letzte Anpassungen

---
+++ public/source/js/menu.js
@@ -0,0 +1,13 @@
+function menu (){
+    let navmenu = document.getElementById('navmenu');
+
+    if (navmenu.style.display === 'block'){
+        navmenu.style.display = 'none';
+    }else{
+        navmenu.style.display = 'block';
+    }
+
+
+
+}
+
